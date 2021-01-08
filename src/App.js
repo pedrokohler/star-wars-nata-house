b820ca2ed629964d99cd801c3e9cba8e040adfc2
Pedro Köhler <pedrokohlerbh@gmail.com>
2021-01-08 10:15:23 -0300

    Minor refactoring and other details

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-import { useEffect, useState } from 'react';
-
-import { fetchShips } from './services/ships';
-import { computeMinimumStops } from './domain/ships';
+import Home from './pages/home';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [page/* , setPage */] = useState(1);
-  const [ships, setShips] = useState([]);
-
-  useEffect(() => {
-    async function loadShips() {
-      const { results, count } = await fetchShips(page);
-      setCount(count);
-      setShips(results);
-    }
-    loadShips();
-  }, [page]);
-
-
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Total: {count}</h1>
-        {ships.map((ship, i) => <div key={i}>
-          <p>{ship.name}</p>
-          <p>{computeMinimumStops(1_000_000, ship)}</p>
-        </div>)}
-      </header>
+      <Home />
     </div>
   );
 }
