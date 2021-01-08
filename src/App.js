fd012c8259972c0e95430a0ec6dba42493a594d1
Pedro Köhler <pedrokohlerbh@gmail.com>
2021-01-08 09:02:10 -0300

    Create first integration and simple display

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+
+import { fetchShips } from './services/ships';
+import { computeMinimumStops } from './domain/ships';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [page/* , setPage */] = useState(1);
+  const [ships, setShips] = useState([]);
+
+  useEffect(() => {
+    async function loadShips() {
+      const { results, count } = await fetchShips(page);
+      setCount(count);
+      setShips(results);
+    }
+    loadShips();
+  }, [page]);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Total: {count}</h1>
+        {ships.map((ship, i) => <div key={i}>
+          <p>{ship.name}</p>
+          <p>{computeMinimumStops(1_000_000, ship)}</p>
+        </div>)}
       </header>
     </div>
   );
